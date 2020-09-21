07dd53e11e6e6a41ed10ef263c2c62a728324d22
eb3095 <ebennerit@gmail.com>
2020-09-21 13:10:50 -0400

    More fixes

--- install.sh
+++ install.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+# Functions
+
+function setupInstaller {
+    sed -i -e 's/# %wheel ALL=(ALL) NOPASSWD\: ALL/%wheelnpw ALL=(ALL) NOPASSWD\: ALL/' /etc/sudoers
+    groupadd wheelnpw
+    useradd installer
+    usermod -aG wheelnpw installer
+    mkdir /home/installer
+    chown installer:installer /home/installer
+
+    # Install trizen
+    pushd /tmp
+    git clone https://aur.archlinux.org/trizen.git
+    popd
+    chmod -R 777 /tmp/trizen
+    runuser -l installer -c 'cd /tmp/trizen; makepkg -si --noconfirm'
+    rm -rf /tmp/trizen
+}
+
+function install_pkgs {
+    packs=$@
+    runuser -l installer -c "trizen -Sy --noconfirm ${packs}"
+}
+
+function removeInstaller {
+    userdel installer
+    rm -rf /home/installer
+}
+
 # Backup
 mkdir -p ~/.config/master-desktop/backup
 cp -rf ~/.config ~/.config2
@@ -7,12 +36,19 @@ cp -rf ~/.config2 ~/.config/master-desktop/backup/.config
 cp -rf ~/.bashrc ~/.config/master-desktop/backup/
 
 # Install dependencies
-trizen -S --noconfirm lxappearance gnome-control-center xorg-xfdttf-font-awesome feh dunst rofi \
-python-i3ipc i3lock dmenu i3-gaps siji-git nerd-fonts-complete inotify-tools maim redshift \
+install_pkgs lxappearance gnome-control-center xorg-xfdttf-font-awesome feh dunst rofi \
+python-i3ipc i3lock dmenu i3-gaps siji-git inotify-tools maim redshift \
 picom-git tilix kdeconnect ethtool nmap net-tools jq kvantum-qt5 imagemagick xdotool \
 xclip dolphin gotop papirus-icon-theme dialog systemsettings python-papirus
 
 pip install python-mdp2
+
+# Setup nerd fonts
+mv /tmp /tmp2
+mkdir /tmp
+install_pkgs nerd-fonts-complete
+/bin/rm -rf /tmp
+mv /tmp2 /tmp
 
 # Get arguments
 ARG1=$1
@@ -80,6 +116,9 @@ sed -i -e 's/bobby/powerline/g' ~/.bashrc
 cp -rf ./dotfiles/.config ~/
 cp -rf ./dotfiles/.icons ~/
 cp -rf ./dotfiles/.themes ~/
+
+# Remove installer
+removeInstaller
 
 # Done!
 echo "Master Desktop installed. Logout and choose i3 in your login screen."
